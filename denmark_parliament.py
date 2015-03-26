b3bb1b3cf43e94ffececd40de90fa022b4861d6a
Oleg Koval <kvl.olg@gmail.com>
2015-03-26 03:44:48 +0200

    start work

--- denmark_parliament.py
+++ denmark_parliament.py
@@ -24,7 +24,7 @@ def _create_entity():
 
 def _get_parties(url):
     party_objects = []
-    party_entities = []
+    _party_entities = []
 
     main_page = BeautifulSoup(urllib2.urlopen(url))
 
@@ -41,18 +41,23 @@ def _get_parties(url):
         party_entity['_meta']['id'] = party_id
         party_entity['_meta']['entity_type'] = 'political party'
         party_entity['name'] = party_name
-        party_entity['fields'].append({'name': 'political_party', 'value': party_name})
-        party_entity['fields'].append({'name': 'number_of_members', 'value': quantity})
 
-        party_entities.append(party_entity)
+        fields = [
+            {'tag': 'political_party', 'value': party_name},
+            {'tag': 'number_of_members', 'value': quantity}
+        ]
 
-    return party_objects, party_entities
+        party_entity['fields'] = fields
+        _party_entities.append(party_entity)
+
+    return party_objects, _party_entities
 
 
 parties_info, party_entities = _get_parties(_base_url)
 
 
 def _get_people(party_obj):
+    people_obj = []
     for party in party_obj:
         modified_url = party['party_url']+PAGINATION
         page_object = BeautifulSoup(urllib2.urlopen(modified_url))
@@ -62,23 +67,43 @@ def _get_people(party_obj):
             person_ulr = _site_ulr+person['onclick'].replace('document.location=(\'', '').replace('\')', '')
             person_id = person_ulr.split('/')[-1].strip('.aspx')
             all_td = person.find_all('td')
-            print all_td
             person_name = ' '.join([_.text for _ in all_td[:2]])
             position = all_td[3].text
             phone = all_td[4].text.split(':')[-1].strip()
-            profile_pic = _site_ulr+all_td.find('img')['src'].replace('~', '')
+            profile_pic = _site_ulr+'/'+all_td[-1].find('img')['src']
 
             people_entity = _create_entity()
             people_entity['_meta']['id'] = person_id
             people_entity['_meta']['entity_type'] = 'person'
             people_entity['name'] = person_name
 
-            people_entity['fields'].append({'name': 'political_party', 'value': party['name']})
-            people_entity['fields'].append({'name': 'url', 'value': person_ulr})
-            people_entity['fields'].append({'name': 'picture_url', 'value': profile_pic})
-            people_entity['fields'].append({'name': 'position', 'value': position})
-            people_entity['fields'].append({'name': 'phone_number', 'value': phone})
+            fields = [{'tag': 'political_party', 'value': party['party_name']},
+                      {'tag': 'url', 'value': person_ulr},
+                      {'tag': 'picture_url', 'value': profile_pic},
+                      {'tag': 'position', 'value': position},
+                      {'tag': 'phone_number', 'value': phone}]
 
+            open_person_url = BeautifulSoup(urllib2.urlopen(person_ulr))
+            bio = open_person_url.find('div', {'class': 'tabContent clearfix'})
+            first_block = bio.find('p').text
+            regexp_born_in_place = re.compile('born (.+),')
+            regexp_born = re.compile(r'born (.+).')
 
+            try:
+                born_string = regexp_born_in_place.search(first_block).group(0).split(',')[0].strip('born ')
+            except AttributeError:
+                born_string = regexp_born.search(first_block).group(0).split(',')[0].split('.')[0].strip('born ')
 
-print _get_people(parties_info)
+            if 'in' in born_string:
+                date, place = born_string.split('in')
+                fields.append({'tag': 'date_of_birth', 'value': date})
+                fields.append({'tag': 'place_of_birth', 'value': place})
+            else:
+                fields.append({'tag': 'date_of_birth', 'value': born_string})
+
+            people_entity['fields'] = fields
+            pprint(people_entity)
+            people_obj.append(people_entity)
+    return people_obj
+
+pprint(_get_people(parties_info))
