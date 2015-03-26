50a8cbe8e1965fbccd0ac53687e1ad23b8dec691
redbonesmith <okoval@lvivsoft.com>
2015-03-26 11:38:23 +0200

    ready_for_home_debug

--- denmark_parliament.py
+++ denmark_parliament.py
@@ -1,11 +1,10 @@
-import itertools
-import helpers
 import re
+from urllib import quote
+
 from bs4 import BeautifulSoup
 
-from urllib import quote
-import urllib2
-from pprint import pprint
+import helpers
+
 
 # doc = BeautifulSoup(helpers.fetch_string(_base_url.format(page), cache_hours=6))
 
@@ -20,14 +19,13 @@ def _create_entity():
             "id": "",
             "entity_type": ""
         },
-        "name": "",
+        "person_name": "",
         "fields": []
     }
 
 
 def _get_parties(url):
     party_objects = []
-    _party_entities = []
 
     # main_page = BeautifulSoup(urllib2.urlopen(url))
     main_page = BeautifulSoup(helpers.fetch_string(url, cache_hours=6))
@@ -39,38 +37,21 @@ def _get_parties(url):
         party_id = row.find('a')['href'].split('=')[1].replace('{', '').replace('}', '')
         party_name = row.find('a').text.encode('utf8')
         party_objects.append({'party_url': party_url, 'party_id': party_id, 'party_name': party_name})
-        quantity = row.find('td').text.encode('utf8')
 
-        party_entity = _create_entity()
-        party_entity['_meta']['id'] = party_id
-        party_entity['_meta']['entity_type'] = 'political party'
-        party_entity['name'] = party_name
-
-        fields = [
-            {'tag': 'political_party', 'value': party_name},
-            {'tag': 'number_of_members', 'value': quantity}
-        ]
-
-        party_entity['fields'] = fields
-        _party_entities.append(party_entity)
-
-    return party_objects, _party_entities
-
-
-parties_info, party_entities = _get_parties(_base_url)
+    return party_objects
 
 
 def _get_people(party_obj):
     people_obj = []
     for party in party_obj:
-        modified_url = party['party_url']+PAGINATION
+        modified_url = party['party_url'] + PAGINATION
         modified_url = quote(modified_url, safe=':/?&=')
         # page_object = BeautifulSoup(urllib2.urlopen(modified_url))
         page_object = BeautifulSoup(helpers.fetch_string(modified_url, cache_hours=6))
 
         table_party = page_object.find('table', {'class': 'telbogTable'}).find_all('tr')[1:]
         for person in table_party:
-            person_ulr = _site_ulr+person['onclick'].replace('document.location=(\'', '').replace('\')', '')
+            person_ulr = _site_ulr + person['onclick'].replace('document.location=(\'', '').replace('\')', '')
             person_id = person_ulr.split('/')[-1].strip('.aspx')
             all_td = person.find_all('td')
             person_name = ' '.join([_.text for _ in all_td[:2]])
@@ -78,14 +59,14 @@ def _get_people(party_obj):
             phone = all_td[4].text.split(':')[-1].strip()
 
             try:
-                profile_pic = _site_ulr+'/'+all_td[-1].find('img')['src']
+                profile_pic = _site_ulr + '/' + all_td[-1].find('img')['src']
             except TypeError:
                 profile_pic = None
 
             people_entity = _create_entity()
             people_entity['_meta']['id'] = person_id
             people_entity['_meta']['entity_type'] = 'person'
-            people_entity['name'] = person_name
+            people_entity['person_name'] = person_name
 
             fields = [{'tag': 'political_party', 'value': party['party_name']},
                       {'tag': 'url', 'value': person_ulr},
@@ -117,18 +98,13 @@ def _get_people(party_obj):
                 fields.append({'tag': 'date_of_birth', 'value': born_string})
 
             people_entity['fields'] = fields
-            pprint(people_entity)
             people_obj.append(people_entity)
     return people_obj
 
-# pprint(_get_people(parties_info))
-
 
 def main():
-    for entity in _get_people(parties_info):
+    for entity in _get_people(_get_parties(_base_url)):
         helpers.emit(entity)
-    for _entity in party_entities:
-        helpers.emit(_entity)
 
 # main scraper
 if __name__ == "__main__":
