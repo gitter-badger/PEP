cc87d96fd9ec8b6cddca49a15073a8999cf95979
LVIVSOFT\vgomeniuk <vgomeniuk@lvivsoft.com>
2015-04-30 17:17:34 +0300

    Cosmetic changes

--- dev_scrapers/jordan.py
+++ dev_scrapers/jordan.py
@@ -4,6 +4,9 @@ from dateutil import parser
 from utils import create_entity, create_id, custom_opener
 import helpers
 
+_MONTHS = {
+    u' \u062d\u0632\u064a\u0631\u0627\u0646': '06'
+}
 
 _host = 'http://www.senate.jo'
 
@@ -31,56 +34,64 @@ def get_all_persons(url):
     rows = page.find('table', {'dir': 'rtl'}).find_all('tr')
 
     for row in rows:
+
         person = {}
         name_two, name_one, url, name = (None, )*4
-        a_href = row.find('a')
-        try:
-            name = a_href.text
-        except:
-            names = row.find_all('td')
-            _, name_one, _, name_two = [_.text.strip(u' \xa0*') for _ in names]
+        for a_href in row.find_all('a'):
+            try:
+                name = a_href.text
+            except:
+                names = row.find_all('td')
+                _, name_one, _, name_two = [_.text.strip(u' \xa0*') for _ in names]
 
-        if a_href:
-            url = a_href.get('href')
+            if a_href:
+                url = a_href.get('href')
 
-        if name:
-            person[PER_NAME] = name.strip(u'  \xa0*')
-        if name_one:
-            person[PER_NAME] = name_one
-        if name_two:
-            person[PER_NAME] = name_two
-        if url:
-            person[PERSON_URL] = url
-            if 'http' not in url:
-                url = _host + url
-            sub_page = custom_opener(url)
-            person[PIC_URL] = _host + sub_page.find('div', {'class': 'senator-image'}).find('img').get('src')
-            table = sub_page.find('table', {'class': 'senator-profile-table'})
-            for _row in table.find_all('tr'):
-                label = _row.find('h3', {'class': 'field-label'})
-                if label:
-                    if label and u'إسم الشهرة:' in label:
-                        person[ALI] = _row.find('div', {'class': 'field-item'}).text.strip()
-                    if label and u'مكان الولادة:' in label:
-                        person[PLACE_BIRTH] = _row.find('div', {'class': 'field-item'}).text.strip()
+            if name:
+                person[PER_NAME] = name.strip(u'  \xa0*')
 
-                    if label and u'تاريخ الميلاد:' in label:
-                        date = _row.find('div', {'class': 'field-item'}).text.strip().strip('.')
-                        separators = ['/']
-                        for sep in separators:
-                            if sep in date:
-                                date = date.split(sep)
-                                date = '{YYYY}-{MM}-{dd}'.format(YYYY=date[-1], MM=date[1], dd=date[0])
-                                date = str(parser.parse(date)).split(' ')[0]
+            if name_one:
+                person[PER_NAME] = name_one
+            if name_two:
+                person[PER_NAME] = name_two
+            if url:
+                person[PERSON_URL] = url
+                if 'http' not in url:
+                    url = _host + url
+                sub_page = custom_opener(url)
+                person[PIC_URL] = _host + sub_page.find('div', {'class': 'senator-image'}).find('img').get('src')
+                table = sub_page.find('table', {'class': 'senator-profile-table'})
+                for _row in table.find_all('tr'):
+                    label = _row.find('h3', {'class': 'field-label'})
+                    if label:
+                        if label and u'إسم الشهرة:' in label:
+                            person[ALI] = _row.find('div', {'class': 'field-item'}).text.strip()
+                        if label and u'مكان الولادة:' in label:
+                            person[PLACE_BIRTH] = _row.find('div', {'class': 'field-item'}).text.strip()
 
-                        person[DATE] = date
-        persons.append(person)
+                        if label and u'تاريخ الميلاد:' in label:
+                            date = _row.find('div', {'class': 'field-item'}).text.strip().strip('.')
+                            separators = ['/']
+                            for sep in separators:
+                                if sep in date:
+                                    date = date.split(sep)
+
+                                    if date[1] in _MONTHS.keys():
+                                        date[1] = _MONTHS[date[1]]
+                                    date = '{YYYY}-{MM}-{dd}'.format(YYYY=date[-1], MM=date[1], dd=date[0])
+                                    date = str(parser.parse(date)).split(' ')[0]
+
+                            person[DATE] = date
+            persons.append(person)
+
     return persons
 
 
 def get_entities(persons):
     entities = []
     for person in persons:
+        if not PER_NAME in person.keys():
+            continue
         name = person.pop(PER_NAME)
         values = person.values()
         unique_id = create_id([_.encode('utf-8') for _ in values])
